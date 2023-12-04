b585afab7837c8b63c99517cd6859877feea5e92
Suraj <surajmhsharma@gmail.com>
2023-12-04 13:19:08 +0530

    responsive design

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { AuthenticationService } from '../../services/authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
 
+  constructor(private authService: AuthenticationService) {}
+
+  login(credentials: any) {
+    this.authService.login(credentials).subscribe(
+      (response) => {
+        console.log('Login successful', response);
+      },
+      (error) => {
+        console.error('Login failed', error);
+      }
+    );
+  }
+  passwordVisible = false;
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent{
+  
+  name: String;
+  email: String;
+  password: String;
 
+  onRegisterSubmit(){
+    const user  = {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }
+
+  }
+
+  constructor(private authService: AuthenticationService) {}
+
+  register(user: any) {
+    this.authService.register(user).subscribe(
+      (response) => {
+        console.log('Registration successful', response);
+      },
+      (error) => {
+        console.error('Registration failed', error);
+      }
+    );
+  }
+  passwordVisible = false;
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
