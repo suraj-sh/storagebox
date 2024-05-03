7965a7caf5fbbe7754b0f23e041de30c543088fb
Suraj Sharma <88436237+suraj-sh@users.noreply.github.com>
2024-05-03 12:09:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'client', 'dist', 'client')));
 
 // Connect to DB
 connectDB();
@@ -50,16 +50,11 @@ app.use(verifyToken);
 app.use('/user',require('./routes/api/user'));
 app.use('/chat',require('./routes/chat'));
 
+
+
 // 404 handler
 app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ error: '404 not found' });
-    } else {
-        res.type('txt').send('404 not found');
-    }
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'client', 'index.html'));
 });
 
 // Overall error handling
