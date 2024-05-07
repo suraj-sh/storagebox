d8cd85274e34549346f699faf69e76a6ea14e25b
Suraj Sharma <88436237+suraj-sh@users.noreply.github.com>
2024-05-07 15:53:33 +0530

    Update corsOptions.js

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,7 +1,5 @@
 //cors origin resource sharing
-const whitelist=['https://storagebox.vercel.app',
-'https://storagebox-l80r7hsw1-surajs-projects-00bb4e84.vercel.app',
-'https://storagebox.onrender.com',
+const whitelist=['https://storagebox.onrender.com',
 'https://storage-box.netlify.app',
 'http://localhost:3500',
 'http://localhost:4200'];
